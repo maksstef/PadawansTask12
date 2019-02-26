63fab79f1f6528bfeb5837330b232076f2a9e93a
mxmstfnk <boss.canta@mail.ru>
2019-02-26 09:16:20 +0300

    1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# PadawansTask12
+﻿## Task description ##
+
+> Implement the algorithm that determines whether all characters (ASCII) in a string occur only once.
+
+[TODO](https://github.com/EPM-RD-NETLAB/Developing-modern-web-applications-with-ASP.NET-and-Microsoft-Azure/blob/master/PadawansToDo.md)
---
+++ StringExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PadawansTask12
+{
+    public static class StringExtension
+    {
+        public static bool AllCharactersAreUnique(string source)
+        {
+            if (source == null)
+                throw new ArgumentNullException();
+            if (source.Length == 0)
+                throw new ArgumentException();
+       
+            for(int i = 0; i < source.Length; ++i)
+            {
+                for(int j = i+1; j < source.Length; ++j)
+                {
+                    if(source[i] == source[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+
+        }
+    }
+}
